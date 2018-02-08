f73866e1b58b05b56cfe01f3c4e2ddb96c9063cf
jonnykalani <jonathankhicks@gmail.com>
2018-02-08 14:10:40 -0500

    Edited index.js
    deleted some junk in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,7 +7,7 @@ const gameEvents = require('./events')
 $(() => {
   setAPIOrigin(location, config)
   $('#space-one').on('click', gameEvents.pushToArrayX(0)
-})
+)
 
 const games = []
 
@@ -35,9 +35,9 @@ const alternator = function () {
 
 // make an event to display an 'X' in spaceOne
 
-let spaceOneXImage = function (
-  $('#spaceOne').val('img file')
-)
+// let spaceOneXImage = function (
+//  $('#spaceOne').val('img file')
+// )
 
 // create a board
 
@@ -99,98 +99,6 @@ const playerTurn = function () {
 const pushToArrayO = function (spaceNum) {
   currentBoardStatus[spaceNum] = 'O'
 }
-
-
-// or I can explicitly write out a function for every single possibility for 'X'
-
-let spaceOneX = function () {
-  currentBoardStatus[0] = 'X'
-}
-
-let spaceTwoX = function () {
-  currentBoardStatus[1] = 'X'
-}
-
-let spaceThreeX = function () {
-  currentBoardStatus[2] = 'X'
-}
-
-let spaceFourX = function () {
-  currentBoardStatus[3] = 'X'
-}
-
-let spaceFiveX = function () {
-  currentBoardStatus[4] = 'X'
-}
-
-let spaceSixX = function () {
-  currentBoardStatus[5] = 'X'
-}
-
-let spaceSevenX = function () {
-  currentBoardStatus[6] = 'X'
-}
-
-let spaceEightX = function () {
-  currentBoardStatus[7] = 'X'
-}
-
-let spaceNineX = function () {
-  currentBoardStatus[8] = 'X'
-}
-
-// And every possibility for 'O'
-
-let spaceOneO = function () {
-  currentBoardStatus[0] = 'X'
-}
-
-let spaceTwoO = function () {
-  currentBoardStatus[1] = 'X'
-}
-
-let spaceThreeO = function () {
-  currentBoardStatus[2] = 'X'
-}
-
-let spaceFourO = function () {
-  currentBoardStatus[3] = 'X'
-}
-
-let spaceFiveO = function () {
-  currentBoardStatus[4] = 'X'
-}
-
-let spaceSixO = function () {
-  currentBoardStatus[5] = 'X'
-}
-
-let spaceSevenO = function () {
-  currentBoardStatus[6] = 'X'
-}
-
-let spaceEightO = function () {
-  currentBoardStatus[7] = 'X'
-}
-
-let spaceNineO = function () {
-  currentBoardStatus[8] = 'X'
-}
-
-
-// this doesn't work
-/*
-let winChecker = function (array) {
-  for (let i = 0; i < winningCombos.length; i++) {
-    if (winningCombos[i] === array) {
-      return 'game over'
-    }
-  }
-}
-*/
-
-
-
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
